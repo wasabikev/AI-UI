b7642f712ea4008677574ca144777c6984638f12
wasabikev <wasabikev@gmail.com>
2023-11-18 16:43:22 -0800

    Updated port configuration for deployment

--- app.py
+++ app.py
@@ -305,5 +305,7 @@ def get_active_conversation():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.getenv('PORT', 8080))  # Get port from PORT environment variable or default to 8080
+    app.run(debug=False, host='0.0.0.0', port=port)
+
 
